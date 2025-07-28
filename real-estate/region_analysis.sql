178d04aada507e1b1ecae15d73f7708e90e3176a
Pavel Fursov <faster.19@mail.ru>
2025-07-28 14:17:28 +0300

    Create region_analysis.sql

---
+++ real-estate/region_analysis.sql
@@ -0,0 +1,63 @@
+WITH limits AS (
+    SELECT  
+        PERCENTILE_DISC(0.99) WITHIN GROUP (ORDER BY total_area) AS total_area_limit,
+        PERCENTILE_DISC(0.99) WITHIN GROUP (ORDER BY rooms) AS rooms_limit,
+        PERCENTILE_DISC(0.99) WITHIN GROUP (ORDER BY balcony) AS balcony_limit,
+        PERCENTILE_DISC(0.99) WITHIN GROUP (ORDER BY ceiling_height) AS ceiling_height_limit_h,
+        PERCENTILE_DISC(0.01) WITHIN GROUP (ORDER BY ceiling_height) AS ceiling_height_limit_l
+    FROM real_estate.flats     
+),
+-- Найдём id объявлений, которые не содержат выбросы:
+filtered_id AS(
+    SELECT id
+    FROM real_estate.flats  
+    WHERE 
+        total_area < (SELECT total_area_limit FROM limits)
+        AND (rooms < (SELECT rooms_limit FROM limits) OR rooms IS NULL)
+        AND (balcony < (SELECT balcony_limit FROM limits) OR balcony IS NULL)
+        AND ((ceiling_height < (SELECT ceiling_height_limit_h FROM limits)
+            AND ceiling_height > (SELECT ceiling_height_limit_l FROM limits)) OR ceiling_height IS NULL)
+    )
+SELECT city,
+		COUNT(*) AS published,
+		ROUND(AVG(total_area)::NUMERIC, 2) AS avg_area,
+        ROUND(AVG(last_price / total_area)::NUMERIC, 2) AS avg_cost_per_sqm
+FROM real_estate.flats
+INNER JOIN filtered_id USING(id)
+LEFT JOIN real_estate.city USING(city_id)
+INNER JOIN real_estate.advertisement USING(id)
+WHERE city != 'Санкт-Петербург'
+GROUP BY city
+ORDER BY published DESC;
+-- Вопрос 4
+-- Определим аномальные значения (выбросы) по значению перцентилей:
+WITH limits AS (
+    SELECT  
+        PERCENTILE_DISC(0.99) WITHIN GROUP (ORDER BY total_area) AS total_area_limit,
+        PERCENTILE_DISC(0.99) WITHIN GROUP (ORDER BY rooms) AS rooms_limit,
+        PERCENTILE_DISC(0.99) WITHIN GROUP (ORDER BY balcony) AS balcony_limit,
+        PERCENTILE_DISC(0.99) WITHIN GROUP (ORDER BY ceiling_height) AS ceiling_height_limit_h,
+        PERCENTILE_DISC(0.01) WITHIN GROUP (ORDER BY ceiling_height) AS ceiling_height_limit_l
+    FROM real_estate.flats     
+),
+-- Найдём id объявлений, которые не содержат выбросы:
+filtered_id AS(
+    SELECT id
+    FROM real_estate.flats  
+    WHERE 
+        total_area < (SELECT total_area_limit FROM limits)
+        AND (rooms < (SELECT rooms_limit FROM limits) OR rooms IS NULL)
+        AND (balcony < (SELECT balcony_limit FROM limits) OR balcony IS NULL)
+        AND ((ceiling_height < (SELECT ceiling_height_limit_h FROM limits)
+            AND ceiling_height > (SELECT ceiling_height_limit_l FROM limits)) OR ceiling_height IS NULL)
+    )
+SELECT city,
+		COUNT(*) AS published,
+		ROUND(AVG(days_exposition::NUMERIC),2) AS duration
+FROM real_estate.flats
+INNER JOIN filtered_id USING(id)
+LEFT JOIN real_estate.city USING(city_id)
+INNER JOIN real_estate.advertisement USING(id)
+WHERE city != 'Санкт-Петербург' AND days_exposition IS NOT NULL
+GROUP BY city
+ORDER BY published DESC, duration;
